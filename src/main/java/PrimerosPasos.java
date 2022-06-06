b8162474745b68b8d846a3b2bc61f5677f2e81a6
Emmanuel <jos.emma3214@gmail.com>
2022-06-05 22:51:13 -0500

    En este commit guardamos la estructura de un for

--- src/main/java/PrimerosPasos.java
+++ src/main/java/PrimerosPasos.java
@@ -9,44 +9,14 @@ public class PrimerosPasos {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    public static void main(String[] args) {
         
-        
-        
-        Scanner leer = new Scanner(System.in);
-        String respuesta;
-        
-        File archivo = new File("C:\\Users\\user01\\Desktop\\Emmanuel\\UAEMEX\\CURSO LOGICA DE PROGRAMACION\\RelacionAudioImagen\\src\\Recursos\\Audio verbos regulares WAV\\1 OPEN.wav");
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(archivo);
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioStream);
-        
-        respuesta = "";
-        
-        while(!respuesta.equals("Q"))
-        {
-            System.out.println("P = PLAY, S = STOP, R = RESET, Q = QUIT");
-            System.out.println("Ingresa la elección ");
-            respuesta = leer.next();
-            respuesta = respuesta.toUpperCase();
-            switch(respuesta)
+
+        //Estructura de un for
+            for(int i = 0; i < 10; i++)
             {
-                case("P"):
-                    clip.start();
-                    System.out.println("La variable es:"+respuesta);
-                break;
-                case("S"):
-                    clip.stop();
-                break;
-                case("R"):
-                    clip.setMicrosecondPosition(0);
-                break;// Por tan solo omitir este break el programa solo ejecutaba una instruccion bien, solamente una vez. 
-                case("Q"):
-                    clip.close();
-                break;
-                default: System.out.println("Respuesta no valida");
+                System.out.println("El iterador vale: " + i);
             }
-            
             
         
         }
@@ -55,4 +25,4 @@ public class PrimerosPasos {
     }
 
     
-}
+
